c6f1722893ae5afa6b6520411c3e1ede86ca1122
Natali-front <natuha1509@icloud.com>
2022-04-18 16:34:22 +0300

    change method to sort linked list

--- script.js
+++ script.js
@@ -1,43 +1,71 @@
 const apiKey = "AIzaSyDtkb7Y94QYrpoWiQ78KkqKv_Bsbcb0txs"
 document.getElementById('request').addEventListener('change', searchYoutube)
 const btn = document.querySelector('.sort')
-
-
+btn.addEventListener('click', function () {
+    setTimeout(makeVideoCards(bubbleSort(list)), 1000)
+})
 
 
 class LinkedList {
     constructor() {
         this.head = null
-        this.tail = null
-    }
+        let length = 0
+        this.size = function () {
+            return length
+        }
 
-    append(data) {
-        const node = { data, next: null }
-
-        if (this.tail) {
-            this.tail.next = node
+        this.add = function (data) {
+            const node = { data, next: null }
+            if (this.head === null) {
+                this.head = node
+            } else {
+                let currentNode = this.head
+                while (currentNode.next) {
+                    currentNode = currentNode.next
+                }
+                currentNode.next = node
+            }
+            length++
         }
-        if (!this.head) {
-            this.head = node
+        this.elementAt = function (index) {
+            let currentNode = this.head
+            let count = 0
+            while (count < index) {
+                count++
+                currentNode = currentNode.next
+            }
+            return currentNode
         }
-        this.tail = node
-    }
-    toArray() {
-        const array = []
-        let current = this.head
-        while (current) {
-            array.push(current)
-            current = current.next
+        this.remove = function (element) {
+            let currentNode = this.head
+            let previousNode
+            if (currentNode.element !== element) {
+                this.head = currentNode.next
+            } else {
+                while (currentNode.element !== element) {
+                    previousNode = currentNode
+                    currentNode = currentNode.next
+                }
+                previousNode.next = currentNode.next
+            }
+            length--
         }
-        return array
     }
 }
+
 const list = new LinkedList()
+
+function swap(list, a, b) {
+    let temp = list.elementAt(a).data;
+    list.elementAt(a).data = list.elementAt(b).data;
+    list.elementAt(b).data = temp
+}
 
 const Compare = {
     LESS_THAN: -1,
     BIGGER_THAN: 1
 };
+
 function defaultCompare(a, b) {
     if (a === b) {
         return 0;
@@ -45,48 +73,40 @@ function defaultCompare(a, b) {
     return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN;
 }
 
-
-function mergeSort(arr, compare = defaultCompare) {
-    if (arr.length > 1) {
-        const { length } = arr;
-        const middle = Math.floor(length / 2);
-        const left = mergeSort(arr.slice(0, middle), compare)
-        const right = mergeSort(arr.slice(middle, length), compare)
-        arr = merge(left, right, compare);
+function bubbleSort(list, compare = defaultCompare) {
+    for (let i = 0; i < list.size(); i++) {
+        for (let j = 0; j < list.size() - 1 - i; j++) {
+            if (compare(list.elementAt(j).data, list.elementAt(j + 1).data) === Compare.BIGGER_THAN) {
+                swap(list, j, j + 1);
+            }
+        }
     }
-    return arr;
+    console.log(list)
+    return list
+
 }
 
-function merge(left, right, compare) {
-    let i = 0;
-    let j = 0;
-    const result = [];
-    while (i < left.length && j < right.length) {
-        result.push(compare(left[i], right[j]) === compare.LESS_THAN ? left[i++] : right[j++]);
-    }
-    return result.concat(i < left.length ? left.slice(i) : right.slice(j))
-}
-
-function makeVideoCards(arr) {
+function makeVideoCards() {
     let wrapper = document.querySelector('.wrapper')
     if (wrapper) {
         wrapper.innerHTML = ''
     }
-    for (let i = 0; i <= arr.length; i++) {
+    for (let i = 1; i <= list.size(); i++) {
         let videoWrapper = document.createElement('div')
         videoWrapper.className = 'video-wrapper'
         wrapper.appendChild(videoWrapper)
         let videoIframe = document.createElement('iframe')
         videoIframe.className = 'video'
         videoWrapper.appendChild(videoIframe)
-        let videoId = arr.map(item => item.data)[i]
-        videoIframe.src = `http://www.youtube.com/embed/${videoId}?autoplay=1?enablejsapi=1&origin=http://localhost:4200`
+        if (list.elementAt(i)) {
+            let videoId = list.elementAt(i).data
+            videoIframe.src = `http://www.youtube.com/embed/${videoId}?autoplay=1?enablejsapi=1&origin=http://localhost:4200`
+        }
     }
 }
 
-
-
 async function searchYoutube(e) {
+
     let wrapper = document.querySelector('.wrapper')
     if (wrapper) {
         wrapper.innerHTML = ''
@@ -95,20 +115,19 @@ async function searchYoutube(e) {
         let request = e.target.value
         let response = await fetch(`https://www.googleapis.com/youtube/v3/search?q=${request}&title=snippet&order=rating&quotaUser=100&maxResults=100&type=video&key=${apiKey}`)
         let results = await response.json()
+        console.log(results)
         results.items.map(item => {
-            list.append(item.id.videoId)
+            list.add(item.id.videoId)
         })
-        let listOfVideos = list.toArray()
+        // let listOfVideos = list.toArray()
         btn.classList.add('visible')
-        btn.addEventListener('click', function () {
-            setTimeout(makeVideoCards(mergeSort(listOfVideos)), 1000)
-        })
-        makeVideoCards(listOfVideos)
+        makeVideoCards(list)
 
     } catch (error) {
         console.log(error)
     }
 }
+
 
 
 
