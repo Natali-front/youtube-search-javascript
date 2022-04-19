3226224b056f2ff2f5f35bceae975d9a6788a240
Natali-front <natuha1509@icloud.com>
2022-04-19 23:21:23 +0300

    add git plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const { GitRevisionPlugin } = require('git-revision-webpack-plugin')
 
 module.exports = {
     mode: 'development',
@@ -14,7 +15,8 @@ module.exports = {
     new HTMLWebpackPlugin({
         template: './src/index.html'
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new GitRevisionPlugin()
     ],
     module: {
         rules: [
