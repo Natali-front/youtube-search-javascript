ef8b87656e29f2f2018709e69d920accff61c6b6
Natali-front <natuha1509@icloud.com>
2022-04-26 00:37:32 +0300

    add section favorite

--- src/favorites/favorites.js
+++ src/favorites/favorites.js
@@ -1,4 +1,5 @@
 import LinkedList from "../linkedList"
+import { wrapperFavorite } from ".."
 
 export const favoriteList = new LinkedList()
 
@@ -7,6 +8,35 @@ export function makeFavoriteList() {
     favoriteList.add(item)
     )
     console.log(favoriteList)
+}
+export function makeFavoriteVideoCards() {
+    if(wrapperFavorite) {
+        wrapperFavorite.innerHTML= ''
+    }
+    for(let i=1; i<=favoriteList.size(); i++) {
+        let videoFavorite = document.createElement('div')
+        videoFavorite.className = 'video-favorite'
+        wrapperFavorite.appendChild(videoFavorite)
+        let videoIframe = document.createElement('iframe');
+        let garbage = document.createElement('i')
+        garbage.className = "fa-solid fa-trash"
+        let arrowDown = document.createElement('i')
+        arrowDown.className= "fa-solid fa-circle-down"
+        let arrowUp = document.createElement('i')
+        arrowUp.className= "fa-solid fa-circle-up"
+        
+        videoFavorite.appendChild(garbage)
+        videoFavorite.appendChild(arrowDown)
+        videoFavorite.appendChild(arrowUp)
+        videoIframe.className = 'video'
+        videoFavorite.appendChild(videoIframe)
+       
+        if (favoriteList.elementAt(i)) {
+            let videoId = favoriteList.elementAt(i).data
+            garbage.id = favoriteList.elementAt(i).data
+            videoIframe.src = `http://www.youtube.com/embed/${videoId}?autoplay=1?enablejsapi=1&origin=http://localhost:4200`
+        }    
+    }
 }
 
 
--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
-import { makeFavoriteList } from './favorites/favorites'
+import { makeFavoriteList, makeFavoriteVideoCards } from './favorites/favorites'
 
 
 export const wrapper = document.querySelector('.wrapper')
+export const wrapperFavorite = document.querySelector('.favorite-wrapper')
 document.getElementById('request').addEventListener('change', searchYoutube)
 
-
+makeFavoriteList()
+makeFavoriteVideoCards()
 
 wrapper.addEventListener('click',  event => {
         let arr = getIdFromLocalStorage()
@@ -17,7 +19,7 @@ wrapper.addEventListener('click',  event => {
         arr.push(event.target.id)
         localStorage.setItem('myFavoriteList', JSON.stringify(arr))
         makeFavoriteList()
-        
+        makeFavoriteVideoCards()
 })
   function getIdFromLocalStorage() {
       return JSON.parse(localStorage.getItem('myFavoriteList') || '[]')
